16a1cd211b7d285b57c32f667fbef9d6bf3670cd
StupidBoi7 <126649764+StupidBoi7@users.noreply.github.com>
2023-07-10 22:33:33 +0530

    Update cmd.py

--- AniPlay/plugins/cmd.py
+++ AniPlay/plugins/cmd.py
@@ -63,6 +63,6 @@ async def stats2(_, message: Message):
 @app.on_message(filters.command(['help', '']))
 async def start(_, message: Message):
     try:
-        await message.reply_text('To Get Started, Simply Use\n\n(**/search <anime name>,\n/find <anime name>,\n/iwant <anime name>.)\n\n(**HINT:** Choose Server5 for Downloading.)\n\n╔══════════════════════╗\n┣**SEARCH ANIME LIKE THIS-**\n┃/s One Piece ✅\n┃/search Naruto ✅\n┃\n┣**DON‘T SEARCH LIKE THIS -**\n┃/s One Piece Episode 69 ❎\n┃/s Bleach English Dub ❎\n╚══════════════════════╝\n\n@AnimeDownloaderChat_Bot.')
+        await message.reply_text('To Get Started, Simply Use\n\n(**/search <anime name>,\n/find <anime name>,\n/iwant <anime name>.)\n\n(**HINT:**Choose Server5 for Downloading.**')
     except:
         return
