e89d12c9c6cea3965692569627359743cf1f8b2a
Detective Conan <109863157+pokefan56@users.noreply.github.com>
2023-07-27 19:49:39 +0530

    Update __init__.py

--- AniPlay/__init__.py
+++ AniPlay/__init__.py
@@ -3,7 +3,7 @@ from pyrogram.client import Client
 
 app = Client(
     "AniPlay",
-    api_id= int(environ.get("APP_ID", "25695562")),
-    api_hash= environ.get("API_HASH", "0b691c3e86603a7e34aae0b5927d725a"),
-    bot_token= environ.get("TOKEN", "6105757517:AAEi2v-Y1B7GAiR50ED-3qfyoz4ymKLpjo8")
+    api_id= int(environ.get("APP_ID", "9976721")),
+    api_hash= environ.get("API_HASH", "3ef17a8cdb938335bd8ba292e6d816aa"),
+    bot_token= environ.get("TOKEN", "6474235595:AAFr8HaA_PrGFGFaBaTYgygAgfRENICcQs0")
 )
